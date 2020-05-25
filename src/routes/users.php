a1fb96ae46aa2465788a22cc2a30688327216de8
atakde <mehata1997@hotmail.com>
2020-05-25 18:40:47 +0300

    Routes :: bug fix

--- src/routes/users.php
+++ src/routes/users.php
@@ -2,8 +2,6 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-$app = new \Slim\App;
-
 /*
  ** Endpoint for getting all user informations.
  */
