6fc18159700fc9f77eb5d9d92a8fe1a898ffcffc
atakde <mehata1997@hotmail.com>
2020-05-30 02:01:05 +0300

    api :: disable error reporting

--- api/index.php
+++ api/index.php
@@ -2,8 +2,8 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
 
 require '../vendor/autoload.php';
 require '../src/config/config.php';
