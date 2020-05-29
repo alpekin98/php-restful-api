724023e9f468668fd583340fc8fba85217c581d6
atakde <mehata1997@hotmail.com>
2020-05-30 01:26:34 +0300

    debugging

--- src/routes/users.php
+++ src/routes/users.php
@@ -140,6 +140,11 @@ $app->group("/users", function () use ($app) {
 
     $app->post('/resettoken', function (Request $request, Response $response) {
 
+        echo "<pre>";
+        echo "debugg";
+        echo "</pre>";
+        die();
+
         $db = new Db();
         
         $HTTPToken = str_replace("Bearer ", "", $request->getServerParams()["HTTP_AUTHORIZATION"]);
